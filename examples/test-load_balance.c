31fdfdb4611c719eacb56ae10bf726d22979e499
hengike <unnamed3@gmail.com>
2019-01-19 01:18:04 +0100

    Fix test :

--- examples/test-load_balance.c
+++ examples/test-load_balance.c
@@ -5,19 +5,19 @@
 
 fake_cmd_t t4p4s_testcase_test[][RTE_MAX_LCORE] = {
     {
-        {FAKE_PKT, 0, 0, ETH(ETH1A, ETH01), 200, 0, ETH(ETH1A, ETH01, "eb")},
-        {FAKE_PKT, 0, 0, ETH(ETH1A, ETH02), 200, 0, ETH(ETH1A, ETH02, "eb")},
-        {FAKE_PKT, 0, 0, ETH(ETH01, ETH1A),   0, 0, ETH(ETH01, ETH1A, "eb")},
-        {FAKE_PKT, 0, 0, ETH(ETH02, ETH1A),   0, 0, ETH(ETH02, ETH1A, "eb")},
+        {FAKE_PKT, 0, 0, ETH(ETH1A, ETH01), 200, 0, ETH(ETH1A, ETH01)},
+        {FAKE_PKT, 0, 0, ETH(ETH1A, ETH02), 200, 0, ETH(ETH1A, ETH02)},
+        {FAKE_PKT, 0, 0, ETH(ETH01, ETH1A),   0, 0, ETH(ETH01, ETH1A)},
+        {FAKE_PKT, 0, 0, ETH(ETH02, ETH1A),   0, 0, ETH(ETH02, ETH1A)},
 
         FEND,
     },
 
     {
-        {FAKE_PKT, 0, 0, ETH(ETH1A, ETH03), 200, 0, ETH(ETH1A, ETH03, "eb")},
-        {FAKE_PKT, 0, 0, ETH(ETH1A, ETH04), 200, 0, ETH(ETH1A, ETH04, "eb")},
-        {FAKE_PKT, 0, 0, ETH(ETH03, ETH1A),   0, 0, ETH(ETH03, ETH1A, "eb")},
-        {FAKE_PKT, 0, 0, ETH(ETH04, ETH1A),   0, 0, ETH(ETH04, ETH1A, "eb")},
+        {FAKE_PKT, 0, 0, ETH(ETH1A, ETH03), 200, 0, ETH(ETH1A, ETH03)},
+        {FAKE_PKT, 0, 0, ETH(ETH1A, ETH04), 200, 0, ETH(ETH1A, ETH04)},
+        {FAKE_PKT, 0, 0, ETH(ETH03, ETH1A),   0, 0, ETH(ETH03, ETH1A)},
+        {FAKE_PKT, 0, 0, ETH(ETH04, ETH1A),   0, 0, ETH(ETH04, ETH1A)},
 
         FEND,
     },
